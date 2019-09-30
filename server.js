c785460cd880d980f00067525c9ecd84e2f0e8d0
Lucas Robitaille <lrjames237@gmail.com>
2019-09-30 13:18:57 -0500

    prepared for deployment

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const helmet = require('helmet')
 const MOVIES = require('./movies-data-small.json')
 
 const app = express()
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -43,8 +44,18 @@ app.get('/movies', function handleGetMovies(req, res) {
     res.json(response)
 })
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+  })
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-  console.log(`Listening on Port ${PORT}`)
+  
 })
